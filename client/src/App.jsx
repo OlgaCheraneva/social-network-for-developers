7e29cc805a81bd7ed863d064bc2cbd2a552c3b51
Olga <cheraneva197@gmail.com>
2020-04-14 15:56:01 +0500

    Added register and login forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,21 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
     <Router>
         <Fragment>
             <Navbar />
-            <Switch>
-                <Route exact path="/" component={Landing} />
-            </Switch>
+            <Route exact path="/" component={Landing} />
+            <section className="container">
+                <Switch>
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                </Switch>
+            </section>
         </Fragment>
     </Router>
 );
---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,64 @@
+import React, {Fragment, useState} from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
+const Login = (props) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const {email, password} = formData;
+
+    const onChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Sign Into Your Account
+            </p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={onChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={onChange}
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
+    );
+};
+
+Login.propTypes = {};
+
+export default Login;
